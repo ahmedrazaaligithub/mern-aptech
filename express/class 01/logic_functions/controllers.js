41dcc747ecf84f94e2f7194415bde2791afd79ad
Muhammad Ahmed Raza <muhammadahmedraza16@gmail.com>
2025-03-27 10:30:21 +0500

    updated

--- express/class 01/logic_functions/controllers.js
+++ express/class 01/logic_functions/controllers.js
@@ -2,12 +2,10 @@ let Index = async function (req, res) {
   res.send("index page");
   res.end();
 };
-
 let About = async function (req, res) {
   res.send("About page");
   res.end();
 };
-
 let Contact = async function (req, res) {
   res.send("Contact page");
   res.end();
--- express/class 01/server.js
+++ express/class 01/server.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const app = express();
 const router = require("./routes/router");
-
+const PROT =process.env.PORT || 3000
 app.use("/", router);
-app.listen(3000, () => {
+app.listen(PORT, () => {
   console.log("Server is running on port 3000");
 });
